f73a5eac222594a3dc040406be4de535aa0cf605
dipujafar <dipujafarmr@gmail.com.>
2024-11-04 19:01:07 +0600

    update

--- src/app/(user)/(babySitter)/book-request/_components/BookRequests.tsx
+++ src/app/(user)/(babySitter)/book-request/_components/BookRequests.tsx
@@ -7,6 +7,18 @@ import { Button } from "@/components/ui/button";
 import { bookBabySitterData } from "@/utils/bookBabySitterData";
 import { MessageCircleMore } from "lucide-react";
 import { motion } from "framer-motion";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 const BookRequests = () => {
   return (
@@ -36,9 +48,38 @@ const BookRequests = () => {
                 </Button>
 
                 {/* Reject button */}
-                <Button className="w-full bg-primary-orange hover:bg-primary-gray group">
-                  Reject
-                </Button>
+
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button className="w-full bg-primary-orange hover:bg-primary-gray group">
+                      Reject
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogDescription className="text-2xl text-center text-primary-blue font-medium mt-5">
+                        Why do you want to reject this appointment?
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <div>
+                        <Textarea
+                          className=""
+                          rows={8}
+                          placeholder="write here reject issue"
+                        ></Textarea>
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <Button
+                        type="submit"
+                        className="w-full bg-primary-orange"
+                      >
+                        Save changes
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
 
                 {/* Approved button */}
                 <Button className="w-full bg-primary-orange hover:bg-primary-gray group">
